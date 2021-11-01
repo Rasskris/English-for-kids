5d5a151077dffd711f5b2b59075e9b14a14ac998
Rasskris <rassoshenko.kristina@yandex.ru>
2021-11-01 16:12:48 +0300

    EFK-FE-8 feat: add game mode switcher

--- efk-fe/src/utils/index.ts
+++ efk-fe/src/utils/index.ts
@@ -1,2 +1,4 @@
 export { getFormData } from './getFormData';
 export { isString } from './isString';
+export { isGameModePlay } from './isGameModePlay';
+export { isGameModeTrain } from './isGameModeTrain';
---
+++ efk-fe/src/utils/isGameModePlay.ts
@@ -0,0 +1,3 @@
+import { GAME_MODE } from '../constants';
+
+export const isGameModePlay = (gameMode: GAME_MODE): boolean => gameMode === GAME_MODE.PLAY;
