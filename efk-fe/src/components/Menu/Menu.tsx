16de6d68b6c36cac5c0c88928d683c7d70b57e3e
Rasskris <rassoshenko.kristina@yandex.ru>
2021-11-14 18:29:31 +0300

    EFK-FE-12 feat: add close menu feature

--- efk-fe/src/components/Menu/Menu.tsx
+++ efk-fe/src/components/Menu/Menu.tsx
@@ -1,36 +1,69 @@
-import { FC, useEffect } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import ClickAwayListener from '@mui/core/ClickAwayListener';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getAllCategories } from '../../redux/thunks';
 import { selectCategories } from '../../redux/selectors';
 import { MenuItem } from './MenuItem';
-import { MAIN_ICON_PATH } from '../../constants';
+import { ICON_PATH } from '../../constants';
 import styles from './Menu.module.scss';
 
 export const Menu: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { asPath } = useRouter();
   const categories = useAppSelector(selectCategories);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getAllCategories());
-  }, [dispatch, categories]);
+  }, []);
+
+  const handleChange = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const handleCloseMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <nav className={styles.menu}>
-      <input className={styles.menu__checkbox} type="checkbox" />
-      <span className={styles.menu__line} />
-      <span className={styles.menu__line} />
-      <span className={styles.menu__line} />
-      <ul className={styles.list}>
-        <MenuItem isActive={asPath === '/'} name="main" iconPath={MAIN_ICON_PATH} routePath="/" />
-        {categories.map(({ id, name, icon }) => {
-          const path = `/category/${id}`;
-          return (
-            <MenuItem key={id} isActive={asPath === path} name={name} iconPath={icon.url} routePath={path} />
-          );
-        })}
-      </ul>
-    </nav>
+    <ClickAwayListener onClickAway={handleCloseMenu}>
+      <nav className={styles.menu}>
+        <input className={styles.menu__checkbox} type="checkbox" checked={isOpen} onChange={handleChange} />
+        <span className={styles.menu__line} />
+        <span className={styles.menu__line} />
+        <span className={styles.menu__line} />
+        <ul className={styles.list}>
+          <MenuItem
+            isActive={asPath === '/'}
+            name="main"
+            iconPath={ICON_PATH.MAIN}
+            routePath="/"
+            onCloseMenu={handleCloseMenu}
+          />
+          <MenuItem
+            isActive={asPath.includes('admin')}
+            name="admin"
+            iconPath={ICON_PATH.ADMIN}
+            routePath="/admin"
+            onCloseMenu={handleCloseMenu}
+          />
+          {categories.map(({ id, name, icon }) => {
+            const path = `/category/${id}`;
+            return (
+              <MenuItem
+                key={id}
+                isActive={asPath === path}
+                name={name}
+                iconPath={icon.url}
+                routePath={path}
+                onCloseMenu={handleCloseMenu}
+              />
+            );
+          })}
+        </ul>
+      </nav>
+    </ClickAwayListener>
   );
 };
--- efk-fe/src/components/Menu/MenuItem.tsx
+++ efk-fe/src/components/Menu/MenuItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, KeyboardEvent } from 'react';
 import Link from 'next/link';
 import classnames from 'classnames';
 import styles from './Menu.module.scss';
@@ -8,17 +8,36 @@ interface MenuItemProps {
   name: string;
   iconPath: string;
   routePath: string;
+  onCloseMenu: VoidFunction;
 }
-export const MenuItem: FC<MenuItemProps> = ({ isActive, name, iconPath, routePath }) => {
+export const MenuItem: FC<MenuItemProps> = ({ isActive, name, iconPath, routePath, onCloseMenu }) => {
   const classes = classnames(styles.list__item, {
     [styles.list__item_active]: isActive,
   });
+
+  const handleClick = () => {
+    onCloseMenu();
+  };
+
+  const handleEnterPress = ({ key }: KeyboardEvent<HTMLAnchorElement>) => {
+    if (key === 'Enter') {
+      onCloseMenu();
+    }
+  };
 
   return (
     <li className={classes}>
       <span className={styles.list__itemImg} style={{ backgroundImage: `url(${iconPath})` }} />
       <Link href={routePath}>
-        <a className={styles.list__itemLink}>{name}</a>
+        <a
+          className={styles.list__itemLink}
+          onClick={handleClick}
+          role="menuitem"
+          tabIndex={0}
+          onKeyPress={handleEnterPress}
+        >
+          {name}
+        </a>
       </Link>
     </li>
   );
