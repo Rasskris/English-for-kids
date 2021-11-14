82178a4d2be6c7a87d82a9408b374352acde3454
Rasskris <rassoshenko.kristina@yandex.ru>
2021-11-14 18:38:07 +0300

    EFK-FE-12 chore: add index fil for export file

---
+++ efk-fe/src/components/Words/index.ts
@@ -0,0 +1,3 @@
+export { WordsContainer } from './WordsContainer/WordsContainer';
+export { WordEditCard } from './WordAdminCard/WordEditCard';
+export { WordAddCard } from './WordAdminCard/WordAddCard';
