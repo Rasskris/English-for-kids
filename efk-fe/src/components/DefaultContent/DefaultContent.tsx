615e070f806d3911253c255a8cedb6c991371820
Rasskris <rassoshenko.kristina@yandex.ru>
2021-11-22 16:47:49 +0300

    EFK-FE-13 feat: add default content component

---
+++ efk-fe/src/components/DefaultContent/DefaultContent.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import Image from 'next/image';
+import { PAGE_DEFAULT_CONTENT, PAGE } from '../../constants';
+import styles from './DefaultContent.module.scss';
+
+interface DefaultContentProps {
+  pageName: PAGE;
+}
+
+export const DefaultContent: FC<DefaultContentProps> = ({ pageName }) => {
+  const { text, image } = PAGE_DEFAULT_CONTENT[pageName];
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>{text}</p>
+      <Image width={500} height={500} quality={100} placeholder="blur" blurDataURL={image} src={image} />
+    </div>
+  );
+};
