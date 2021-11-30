70380aa9fdbc98fbf094de63da041f2493cb391e
Rasskris <rassoshenko.kristina@yandex.ru>
2021-11-30 14:18:33 +0300

    chore: add index file for export files

--- efk-fe/src/components/Words/index.ts
+++ efk-fe/src/components/Words/index.ts
@@ -1,3 +1,4 @@
 export { WordsContainer } from './WordsContainer/WordsContainer';
 export { WordEditCard } from './WordAdminCard/WordEditCard';
 export { WordAddCard } from './WordAdminCard/WordAddCard';
+export { WordCard } from './WordCard/WordCard';
--- efk-fe/src/components/index.ts
+++ efk-fe/src/components/index.ts
@@ -1,6 +1,6 @@
 export { Layout } from './Layout';
 export { CategoryCard, CategoryEditCard, CategoryAddCard } from './Categories';
-export { WordsContainer, WordAddCard, WordEditCard } from './Words';
+export { WordsContainer, WordAddCard, WordEditCard, WordCard } from './Words';
 export { GameButtonsContainer } from './GameButtonsContainer';
 export { RatingLine } from './RatingLine';
 export { Modal } from './Modal';
@@ -10,3 +10,6 @@ export { AuthFormWrapper } from './FormControls';
 export { CardsContainer } from './CardsContainer';
 export { Table } from './Table';
 export { DefaultContent } from './DefaultContent';
+export { Menu } from './Menu';
+export { Header } from './Layout';
+export { Switcher } from './Switcher';
