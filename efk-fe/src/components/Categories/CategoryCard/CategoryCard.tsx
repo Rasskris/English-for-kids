ae28f6fb67d80efba921d1518d98286b18879f15
Rasskris <rassoshenko.kristina@yandex.ru>
2021-12-20 15:12:04 +0300

    EFK-FE-24 feat: add alt to image

--- efk-fe/src/components/Categories/CategoryCard/CategoryCard.tsx
+++ efk-fe/src/components/Categories/CategoryCard/CategoryCard.tsx
@@ -9,11 +9,24 @@ interface CategoryCardProps {
 }
 
 export const CategoryCard: FC<CategoryCardProps> = memo(({ category }) => {
-  const { id, name, coverImage } = category;
+  const {
+    id,
+    name,
+    coverImage: { url: imageUrl },
+  } = category;
 
   return (
     <figure className={styles.card} data-category-name={name} role="figure">
-      <Image className={styles.card__img} width={250} height={250} src={coverImage.url} quality={100} />
+      <Image
+        className={styles.card__img}
+        placeholder="blur"
+        blurDataURL={imageUrl}
+        width={250}
+        height={250}
+        src={imageUrl}
+        quality={100}
+        alt={name}
+      />
       <figcaption className={styles.card__name}>{name}</figcaption>
       <Link href={`/category/${id}/`}>
         <a className={styles.card__link} />
